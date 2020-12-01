eb972c444a1a9f126e5e847d9eebfcca0d1ff9d0
Lonnie Souder II <lonnie2@souderfamily.net>
2020-12-01 17:44:36 -0500

    added state changed and fixed button alignment

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,10 +2,20 @@ import React from 'react'
 import './Square.css'
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
         return (
-            <button className='square'>
-                ({this.props.col}, {this.props.row})
+            <button
+                className='square'
+                onClick={() => {this.setState({value: 'X'})}}
+            >
+                    {this.state.value}
             </button>
         )
     }
