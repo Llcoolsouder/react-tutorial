1eface7a4f6a53a80db0300dd3703f4a4e5cda03
Lonnie Souder II <lonnie2@souderfamily.net>
2020-12-02 13:10:44 -0500

    alternates players

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,33 +2,64 @@ import React from 'react'
 import Square from './Square'
 
 class Board extends React.Component {
-    renderSquare(col, row) {
-      return <Square col={col} row={row} />
-    }
-  
-    renderRowOfSquares(rowIndex, length) {
-      var squares = []
-      for (var col = 0; col < length; col++) {
-        squares.push(this.renderSquare(col, rowIndex));
-      }
-      return (
-        <div className='row-of-squares'>
-          {squares}
-        </div>
-      );
-    }
-  
-    render() {
-      var rows = []
-      for (var row = 0; row < 3; row++) {
-        rows.push(this.renderRowOfSquares(row, 3))
-      }
-      return (
-        <div className='board'>
-          {rows}
-        </div>
-      );
+  constructor(props) {
+    super(props)
+    this.ROWS = 3
+    this.COLS = 3
+
+    this.state = {
+      squares: Array.from(Array(this.ROWS), () => new Array(this.COLS)),
+      marker: 'X'
     }
   }
 
-  export default Board;
+  getNextMarker() {
+    if (this.state.marker === 'X') {
+      return 'O'
+    }
+    else {
+      return 'X'
+    }
+  }
+
+  handleClick(col, row) {
+    let squares = this.state.squares.map((row) => row.slice())
+    squares[row][col] = this.state.marker
+    this.setState({squares: squares, marker: this.getNextMarker()})
+  }
+
+  renderSquare(col, row) {
+    return (
+      <Square
+        value={this.state.squares[row][col]}
+        onClick={() => this.handleClick(col, row)}
+      />)
+  }
+  
+  renderRowOfSquares(rowIndex, length) {
+    let squares = []
+    for (var col = 0; col < length; col++) {
+      squares.push(this.renderSquare(col, rowIndex))
+    }
+    return (
+      <div className='row-of-squares'>
+        {squares}
+      </div>
+    )
+  }
+
+  render() {
+    let rows = []
+    for (var row = 0; row < this.ROWS; row++) {
+      rows.push(this.renderRowOfSquares(row, this.COLS))
+    }
+    return (
+      <div className='board'>
+        <h1>The next player is: {this.state.marker}</h1>
+        {rows}
+      </div>
+    )
+  }
+  }
+
+  export default Board
